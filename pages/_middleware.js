3e549a21f6bb73d08993d868ee6940fc6d7a9c85
banana <echohtp@gmail.com>
2022-10-21 16:39:08 -0400

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server'
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
     if (pathname == '/') {
-        return NextResponse.rewrite(new URL('/7K1eHME4xvusf49PVqzatUJHLqdZMNvBWjVhz6j3D73B', req.url))
+        return NextResponse.rewrite(new URL('https://stake.cardinal.so/7K1eHME4xvusf49PVqzatUJHLqdZMNvBWjVhz6j3D73B', req.url))
         // return NextResponse.redirect('/7K1eHME4xvusf49PVqzatUJHLqdZMNvBWjVhz6j3D73B')
     }
     return NextResponse.next()
