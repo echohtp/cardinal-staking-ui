2ca83faa617054078019656949c031ab657f32ee
Jason Schorr <echohtp@gmail.com>
2022-10-21 16:50:25 -0400

    stake rewrite

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,8 +2,8 @@ import { NextResponse, NextRequest } from 'next/server'
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
     if (pathname == '/') {
-        return NextResponse.rewrite(new URL('https://stake.cardinal.so/7K1eHME4xvusf49PVqzatUJHLqdZMNvBWjVhz6j3D73B', req.url))
-        // return NextResponse.redirect('/7K1eHME4xvusf49PVqzatUJHLqdZMNvBWjVhz6j3D73B')
+        // return NextResponse.rewrite(new URL('https://stake.cardinal.so/7K1eHME4xvusf49PVqzatUJHLqdZMNvBWjVhz6j3D73B', req.url))
+        return NextResponse.redirect('https://stake.cardinal.so/7K1eHME4xvusf49PVqzatUJHLqdZMNvBWjVhz6j3D73B')
     }
     return NextResponse.next()
 }
